70d34c29f2f08412b1e65342404247af1cd4d9b3
Gareth Larkan <69150667+GarethJamesLarkan@users.noreply.github.com>
2022-09-01 21:05:51 +0200

    Create function edits

--- contracts/DAOFactory.sol
+++ contracts/DAOFactory.sol
@@ -5,7 +5,7 @@ contract BridgeFactory {
 
     struct Company {
         uint256 ID;
-        address name;
+        string name;
         address wallet;
         address[] contractInstances;
     }
@@ -14,10 +14,17 @@ contract BridgeFactory {
 
     mapping(uint256 => Company) public companies;
 
+    event CompanyCreated(uint256 companyID, string name, address wallet);
+
     function createCompany(Company memory company) external {
         require(company.wallet != address(0), "Cannot be zero-address");
         company.ID = companyNumber;
         companies[companyNumber] = company;
+        companyNumber++;
+
+        emit CompanyCreated(company.ID, company.name, company.wallet);
     }
+
+
     
 }
